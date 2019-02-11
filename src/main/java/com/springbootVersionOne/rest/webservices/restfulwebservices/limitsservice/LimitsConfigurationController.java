b335a7c0c1b6be50d8d7f3757b08f6dc7effcae0
fenil <jainfenil@gmail.com>
2019-02-10 23:05:39 -0600

    added configuration class to fetch configurations dynamically

--- src/main/java/com/springbootVersionOne/rest/webservices/restfulwebservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/springbootVersionOne/rest/webservices/restfulwebservices/limitsservice/LimitsConfigurationController.java
@@ -1,15 +1,19 @@
 package com.springbootVersionOne.rest.webservices.restfulwebservices.limitsservice;
 
 import com.springbootVersionOne.rest.webservices.restfulwebservices.limitsservice.bean.LimitConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public LimitConfiguration retriveLimitsFromConfiguration(){
 
-        return new LimitConfiguration(1000,1);
+        return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
     }
 }
